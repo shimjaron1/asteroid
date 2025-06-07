1e140864a601b4ab4b92c129b90a0047404e4da3
learningsre1028 <215057158+learningsre1028@users.noreply.github.com>
2025-06-06 21:05:00 -0400

    Add player and circleshape

--- main.py
+++ main.py
@@ -3,19 +3,39 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
+	# initialize to start the game
+	pygame.init()
+
+	# Generates the window
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+	# Create time object used to help control FPS
+	time = pygame.time.Clock()
+	dt = 0
+
+	# Set game loop condition
 	while True:
+		# Check if the user has closed the window and exit the game loop. Allows function
+		# to close generated window
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		pygame.init()
-		pygame.fill("black")
-		print("Starting Asteroids!")
-		print(f"Screen width: {SCREEN_WIDTH}")
-		print(f"Screen height: {SCREEN_HEIGHT}")
-		screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+		
+		screen.fill("black")
+
+		# Draw player
+		player.draw(screen)
+		
+		# Method to refresh screen
 		pygame.display.flip()
+
+		# Return and set delta since .tick() was last called
+		dt = time.tick(60)/1000
 
 if __name__ == "__main__":
 	main()
